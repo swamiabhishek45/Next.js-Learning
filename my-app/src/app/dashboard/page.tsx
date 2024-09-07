b107d4549beaefcd68c8039a8c95a5b3040f9892
Abhishek Swami <abhishekswami1435@gmail.com>
2024-09-07 19:50:00 +0530

    ADD: file colocation

---
+++ my-app/src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+
+// FIXME: Error: The default export is not a React Component in page: "/dashboard"
+function BarChart(){
+    return <h1>Bar Chart</h1>
+}
+
+export default function Dashboard(){
+    return <h1>Dashboard</h1>
+}
