ce28979c02fe346fe401e241b008ca570ffe253c
Abhishek Swami <abhishekswami1435@gmail.com>
2024-09-08 16:29:39 +0530

    ADD: title metadata

--- my-app/src/app/blog/page.tsx
+++ my-app/src/app/blog/page.tsx
@@ -1,3 +1,11 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: {
+        absolute: "Blog" // ignores template from root layoutr
+    }
+}
+
 export default function Blog() {
     return <h1>Blog Page!</h1>;
 }
