4d06ae000646585955b2f2d42f19cc3db4897a72
Abhishek Swami <abhishekswami1435@gmail.com>
2024-09-08 17:03:28 +0530

    ADD: Link component navigation

--- file-routing/src/app/about/page.tsx
+++ file-routing/src/app/about/page.tsx
@@ -1,9 +1,16 @@
+import Link from "next/link";
+
 export const metadata = {
     title: "About Me",
     description: "About me page",
 };
 
 export default function About() {
-    return <h1>About Page!</h1>
+    return (
+        <>
+            <h1>About Page!</h1>
+            <Link href='/about/education'>Education</Link>
+        </>
+    );
 }
 
--- file-routing/src/app/page.tsx
+++ file-routing/src/app/page.tsx
@@ -1,3 +1,21 @@
+import Link from "next/link";
+
 export default function Home(){
-    return <h1>Home Page</h1>
+    return (
+        <>
+            <h1>Home Page</h1>
+
+            <div>
+                <li><Link href="/about">About</Link></li>
+                <li><Link href="/blog">Blog</Link></li>
+                <li><Link href="/dashboard">Dashboard</Link></li>
+                <li><Link href="/docs">Docs</Link></li>
+                <li><Link href="/products">Products</Link></li>
+                <li><Link href="/profile">Profile</Link></li>
+                <li><Link href="/register">Register</Link></li>
+                <li><Link href="/login">Login</Link></li>
+                <li><Link href="/forgot-password">Forgot Password</Link></li>
+            </div>
+        </>
+    );
 }
