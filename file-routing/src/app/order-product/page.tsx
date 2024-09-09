8164af240b9b031161b182750993309cbb2d360d
Abhishek Swami <abhishekswami1435@gmail.com>
2024-09-09 12:57:31 +0530

    ADD: navigating programmatically

---
+++ file-routing/src/app/order-product/page.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+export default function OrderProduct() {
+    const router = useRouter();
+
+    const handleClick = () => {
+        console.log("Placing your order");
+        router.push("/");
+        // router.replace("/");
+        // router.back();
+        // router.forward();
+    };
+    return (
+        <>
+            <h1>Order Product Page</h1>
+
+            <button onClick={handleClick}>Place Order</button>
+        </>
+    );
+}
