c4aa13002f194245f10b5928146236c64a698319
Abhishek Swami <abhishekswami1435@gmail.com>
2024-09-10 11:51:30 +0530

    ADD: template.tsx file

--- file-routing/src/app/(auth)/template.tsx
+++ file-routing/src/app/(auth)/template.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 const navLinks = [
     { name: "Register", href: "/register" },
@@ -14,13 +15,30 @@ export default function AuthLayout({
     children: React.ReactNode;
 }) {
     const pathname = usePathname();
+    const [input, setInput] = useState("");
+
     return (
         <div>
+            <div>
+                <input
+                    type="text"
+                    value={input}
+                    onChange={(e) => setInput(e.target.value)}
+                />
+            </div>
             {navLinks.map((link) => {
                 const isActive = pathname.startsWith(link.href);
                 return (
                     <li>
-                        <Link href={link.href} key={link.name} className={isActive ? "font-bold text-red-400 underline": "text-blue-400"}>
+                        <Link
+                            href={link.href}
+                            key={link.name}
+                            className={
+                                isActive
+                                    ? "font-bold text-red-400 underline"
+                                    : "text-blue-400"
+                            }
+                        >
                             {link.name}
                         </Link>
                     </li>
