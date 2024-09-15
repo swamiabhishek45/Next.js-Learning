9c6911d3ccce0e687a334e0c286a13ae3e726c10
Abhishek Swami <abhishekswami1435@gmail.com>
2024-09-15 22:23:38 +0530

    ADD:error.tsx

---
+++ file-routing/src/app/products/[productId]/feedback/[fid]/error.tsx
@@ -0,0 +1,5 @@
+// "use client";   
+
+export default function ErrorBoundary(){
+    return <div>Error in feedback id</div>
+}
--- file-routing/src/app/products/[productId]/feedback/[fid]/page.tsx
+++ file-routing/src/app/products/[productId]/feedback/[fid]/page.tsx
@@ -1,15 +1,28 @@
 import { notFound } from "next/navigation";
 
+function getRandomInt(count: number) {
+    return Math.floor(Math.random() * count);
+}
+
 export default function Fid({
     params,
 }: {
     params: {
         productId: string;
-        no: string;
+        fid: string;
     };
 }) {
-    if(parseInt(params.no) > 1000){
+    const randomInt = getRandomInt(2);
+    if (randomInt === 1) {
+        throw new Error("Error loading feedback...");
+    }
+    
+    if (parseInt(params.fid) > 1000) {
         notFound();
     }
-    return <h1>Feedback {params.no} of product {params.productId} </h1>;
+    return (
+        <h1>
+            Feedback {params.fid} of product {params.productId}{" "}
+        </h1>
+    );
 }
